9fc81a0307d083d0821af2d5839886e45ca6a61e
Karan300666 <karan3072005@gmail.com>
2026-07-05 17:22:54 +0530

    change in prompt

--- backend/src/controllers/AI.controller.ts
+++ backend/src/controllers/AI.controller.ts
@@ -31,27 +31,27 @@ export async function creatingInterview(req: Request , res: Response){
     try {
 
         
-    //     const AIresponse = await getAIResponse(
-    //         `Prepare questions for a job interview.
-    //     The job role is ${role}.
-    //     The job experience level is ${level}.
-    //     The tech stack used in the job is: ${techStack}.
-    //     The focus between behavioural and technical questions should lean towards: ${type}.
-    //     The amount of questions required is: ${amount}.
-    //     Please return only the questions, without any additional text.
-    //     The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
-    //     Return the questions formatted like this:
-    //     ["Question 1", "Question 2", "Question 3"]
-    // `
-    //     )
+        const AIresponse = await getAIResponse(
+            `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techStack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+    `
+        )
 
-    //     if(!AIresponse){
-    //     return res.status(500).json({
-    //         message: "AI can't create questions" 
-    //     })
-    //     }
-     //   const AIResponseArray = AIresponse
-    //                       .split(",")
+        if(!AIresponse){
+        return res.status(500).json({
+            message: "AI can't create questions" 
+        })
+        }
+       const AIResponseArray = AIresponse
+                          .split(",")
         const interview = await prisma.interview.create({
             data: {
                 userId,
@@ -60,7 +60,7 @@ export async function creatingInterview(req: Request , res: Response){
                 type,
                 amount,
                 techStack: techStackArray,
-                question: ["what is jwt" , "what is sql"],
+                question: AIResponseArray,
             }
         })
         return res.status(200).json({
