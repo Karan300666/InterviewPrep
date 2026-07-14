2c362de9c83b6709700ab376b4ca1a84837c8d5f
Karan300666 <karan3072005@gmail.com>
2026-07-14 15:11:32 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   async rewrites() {
+        return [
+          {
+            source: 'api/:path*",
+            destination: " https://interviewprep-ir1z.onrender.com"
+          }
+        ]
+   }
 };
 
 export default nextConfig;
